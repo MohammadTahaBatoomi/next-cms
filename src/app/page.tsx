9bd061d0925a4a85c54685520fdec0b21933ab58
Mohammad Taha <tbatoomi@gmail.com>
2025-06-28 08:38:43 +0330

    add img sm sidbar & header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 "use client"
 import React, { useState } from 'react';
-import Salesoverviewchart from "@/components/sales-overview-chart";
-import Yearlybreakup from "@/components/yearly-breakup";
-import Monthlyearnings from "@/components/monthly-earnings";
-import Recenttransactions from "@/components/recent-transactions";
-import Productperformance from "@/components/product-performance";
-import Product from "@/components/product";
+import Salesoverviewchart from "@/components/landing/sales-overview-chart";
+import Yearlybreakup from "@/components/landing/yearly-breakup";
+import Monthlyearnings from "@/components/landing/monthly-earnings";
+import Recenttransactions from "@/components/landing/recent-transactions";
+import Productperformance from "@/components/landing/product-performance";
+import Product from "@/components/landing/product";
 import SidebarShared from '@/components/shared/sidebar-shared';
 import HeaderShared from '@/components/shared/header-shared';
 
