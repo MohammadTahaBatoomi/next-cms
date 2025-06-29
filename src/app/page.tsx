1e843594d09b57626baf3fd0d8607de75958e57d
Mohammad Taha <tbatoomi@gmail.com>
2025-06-29 21:51:42 +0330

    config landing dashboard but 2 bug in project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,6 +19,26 @@ export default function Home() {
           <HeaderShared onOpenSidebar={() => setMobileSidebarOpen(true)} />
 
             <div className='max-w-6xl mx-auto'>
+            <article className="flex flex-col 2xl:flex-row justify-center items-start max-w-6xl mx-auto gap-4">
+  {/* چارت سمت چپ */}
+  <div className="w-full 2xl:w-2/3">
+    <Salesoverviewchart />
+  </div>
+
+  {/* دو کارت سمت راست */}
+  <div className="w-full flex flex-col sm:flex-col md:flex-row 2xl:flex-col gap-4 2xl:w-1/3">
+    <div className="w-full md:w-1/2">
+      <Yearlybreakup />
+    </div>
+    <div className="w-full md:w-1/2">
+      <Monthlyearnings />
+    </div>
+  </div></article>
+
+            <article className='flex flex-col md:flex-row justify-between items-start gap-6 mb-6'>
+  <Recenttransactions />
+  <Productperformance />
+</article>
                <Blog />
             </div>
 
